6973b20541a7c899c8c4fb52fda3ef9b5e8354b2
Harsh40863 <harshvaishnav0314@gmail.com>
2026-03-01 12:19:52 +0530

    api response

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import { DB_NAME } from "./constants.js"
 import express from "express"
 import connectDB from "./DB/index.js"
 import dotenv from "dotenv"
+import {app} from "./app.js"
+import cors from "cors"
+import cookieParser from "cookie-parse"
+dotenv.config()
 
-dotenv.config({
-    path:"./env"
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("server is running ||||")
+    })
 })
-
-// const app=express()
-
-
-connectDB();
+.catch((err)=>{
+    console.log("mongoDB is not working |||")
+})
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(express.cookieParser())
+app.use
 
