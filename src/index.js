9d75372536074bea26767ce0ae2f7e5d99e95948
Harsh Vaishnav <harshvaishnav@Harshs-MacBook-Air.local>
2026-04-11 21:07:02 +0530

    this is running

--- src/index.js
+++ src/index.js
@@ -3,29 +3,28 @@ import { DB_NAME } from "./constants.js"
 import express from "express"
 import connectDB from "./DB/index.js"
 import dotenv from "dotenv"
-import {app} from "./app.js"
+import { app } from "./app.js"
 import cors from "cors"
-import cookieParser from "cookie-parse"
+import cookieParser from "cookie-parser"
+
 dotenv.config()
 
+// Middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
 
-
-
+// DB + Server start
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log("server is running ||||")
     })
 })
-.catch((err)=>{
-    console.log("mongoDB is not working |||")
+.catch((err) => {
+    console.log("mongoDB is not working |||", err)
 })
-app.use(cors({
-    origin:process.env.CORS_ORIGIN
-}))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(express.cookieParser())
-app.use
-
